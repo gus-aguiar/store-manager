a5846bad7d1de41cebad179cfadf4aaccd38fa70
gus-aguiar <gustavopma90@gmail.com>
2023-04-26 17:42:43 -0300

    req 1

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const productController = require('./controllers/productController');
 
 const app = express();
+app.use(express.json());
+
+
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-//intial commit 
+
+app.get('/products', productController.getAll);
+
+app.get('/products/:id', productController.getById);
+
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
---
+++ src/services/productService.js
@@ -0,0 +1,19 @@
+const productModel = require('../models/productModel');
+
+
+
+const getAll = async () => {
+  const products = await productModel.getAll();
+  return products;
+}
+
+const getById = async (id) => {
+  const product = await productModel.getById(id);
+  if (!product) return { type: 404, message: 'Product not found' };
+  return { type: null, message: product };
+}
+
+module.exports = {
+  getAll,
+  getById,
+};
